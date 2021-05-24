ce0aafdd3f46f7622d35e9e987ad45261223244d
Ozlem Cagirici <64790142+CagiriciOzlem@users.noreply.github.com>
2021-05-25 00:24:29 +0300

    Add files via upload

--- Virtual Environment - Conda/venv_conda.py
+++ Virtual Environment - Conda/venv_conda.py
@@ -1,8 +1,6 @@
 # For creating the 'virtualenv' for the project, use the following command :
 
-conda
-create - n
-vbo_env
+condav create - n vbo_env
 
 """
     vbo_env : name of the new virtual environment to be created
@@ -10,30 +8,21 @@ vbo_env
 
 # For creating the 'virtualenv' with spesific version of python, use the following command :
 
-conda
-create - n
-vbo_env
-python = 3
+conda create - n vbo_env python = 3
 
 # You can use the following command to activate the current environment:
 
-conda
-activate
-vbo_env
+conda activate vbo_env
 
 # Listing all of the installed packages inside the current environment:
 
-conda
-list
+conda list
 
 # Install package with spesific version or last version:
 # - If the version is specified, the package is installed with the relevant version,
 # - if not, the installation is performed with the latest version.
 
-conda
-install
-numpy
-pandas = 1.2
+conda install numpy pandas = 1.2
 .1
 
 """
@@ -42,42 +31,30 @@ pandas = 1.2
 
 # Checking the version of the specific package in current environment (etc:numpy?):
 
-python - c
-"import numpy; print(numpy.__version__)"
+python - c "import numpy; print(numpy.__version__)"
 
 # Checking the list or version of the packages:
 
-conda
-list
+conda list
 
 # For the uninstallation of the package, use the following command (for instance, numpy):
 
-conda
-remove
-numpy
+conda remove numpy
 
 # For performing updates, use the following command: for instance, pandas)
 
-conda
-update
-pandas
+conda update pandas
 
 # Saving the packages to a YAML file:
 
-conda
-env
-export > vbo_env_packages.yaml
+conda env export > vbo_env_packages.yaml
 
 # The following command removes the 'vbo_env' Virtual Environment with all its packages at the same time.
 
 # - Firstly, deactive the current environment.
 
-conda
-deactivate
+conda deactivate
 
 # - Secondly, remove the 'vbo_env' Virtual Environment
 
-conda
-env
-remove - n
-vbo_env
+conda env remove - n vbo_env
